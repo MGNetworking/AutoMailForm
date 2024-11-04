b5b1ac70acd5c431c6eb9295d11ce36907dc10d7
Maxime <ghalem.maxime@gmail.com>
2024-11-04 16:01:57 +0100

    first endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import favicon from "serve-favicon";
 
-console.log("Run this projet ...");
-
 const app = express();
 const port = 3010;
-
+console.log(__dirname);
 // middlewares
 app.use(favicon(__dirname + "/favicon.ico"));
+
+// run API
+app.listen(port, () =>
+  console.log(`this API to run in basic IP: http://localhost:${port}`)
+);
+
+app.get("/api/hello", (req, res) => {
+  res.json("hello world ;)");
+});
